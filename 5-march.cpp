7a990a79a27822c087ebbf5936042178131209bd
Sanskrati Jain <31sanskrati@gmail.com>
2021-03-07 06:46:25 +0530

    Modified 5-march.cpp

--- 5-march.cpp
+++ 5-march.cpp
@@ -3,23 +3,26 @@ using namespace std;
 
 int main()
 {
-//***************for running make each section a comment**************
+    //***************for running make each section a comment**************
 
-//selection control structure: if-else ladder
+    //selection control structure: if-else ladder
     int age;
     cout << "Invitaion to party. Enter you age:" << endl;
     cin >> age;
-    if(age<=16){
-        cout<<"You are a kid. Go to your bed"<<endl;
+    if (age <= 16)
+    {
+        cout << "You are a kid. Go to your bed" << endl;
     }
-    else if(age<19){   //((age>16) && (age<19))
-        cout<<"Study for your enterance exam\n";
+    else if (age < 19)
+    { //((age>16) && (age<19))
+        cout << "Study for your enterance exam\n";
     }
-    else{
-        cout<<"Party starts at 9pm on Sunday"<<endl;
+    else
+    {
+        cout << "Party starts at 9pm on Sunday" << endl;
     }
 
-//switch case structure
+    //switch case structure
     switch (age)
     {
     case 18:
@@ -34,44 +37,42 @@ int main()
     }
     cout << "Follow above statement";
 
-// For loop 
+    // For loop
     for (int i = 0; i < 50; i++)
     {
-        cout<<i<<endl;
+        cout << i << endl;
     }
-    
+
     // Ctrl+c for stobing to run
-// infinite for loop
-    for(int i = 1; 1 < 10; i++)
+    // infinite for loop
+    for (int i = 1; 1 < 10; i++)
     {
-        cout<<i<<endl;
+        cout << i << endl;
     }
 
-//while loop
+    //while loop
     int i = 1;
-    while (i<=50)
+    while (i <= 50)
     {
-        cout<<i<<endl;
-        i++;
-    }
-    
-//Example of infinite while loop
-    int i = 1;
-    while(true)
-    {
-        cout<<i<<endl;
+        cout << i << endl;
         i++;
     }
 
-//do-while loop -->at least once this will run
+    //Example of infinite while loop
     int i = 1;
-    do{
-        cout<<i<<endl;
+    while (true)
+    {
+        cout << i << endl;
         i++;
-    }while(i<=20);
+    }
 
-
-
+    //do-while loop -->at least once this will run
+    int i = 1;
+    do
+    {
+        cout << i << endl;
+        i++;
+    } while (i <= 20);
 
     return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ // for (int i = 0; i < 3; i++)
+    // {
+    //     cout<<"The value of marks "<<i<<" is "<<marks[i]<<endl;
+    // }
