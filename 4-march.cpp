3f609081cf675afeb2fa56a1db486c7586fd5f3c
Sanskrati Jain <31sanskrati@gmail.com>
2021-03-08 21:27:37 +0530

    Manipulators in C++

--- 4-march.cpp
+++ 4-march.cpp
@@ -3,25 +3,26 @@
 using namespace std;
 
 int main(){
-//*********for running make each section a comment**********
+//*********for running the code --> make each section a comment**********
 
 //Constant value**
-    int a = 5; //for making this constant write const int a =5;
-    cout<<"the value of a is "<<a<<endl; 
-    a = 45;
-    cout<<"The new value of a is "<<a;
+    int num = 5; //for making this constant write const int a =5;
+    cout<<"the value of num is "<<a<<endl; 
+    num = 45;
+    cout<<"The new value of num is "<<a;
 
 //Manipulators in C++**
     int a=3, b=738, c=55943;
     cout<<"The value of a is: "<<a<<endl;
     cout<<"The value of b is: "<<b<<endl;
     cout<<"The value of c is: "<<c<<endl;
+    //setw(5) --> will behave like the variable has width of 5
     cout<<"The value of a is: "<<setw(5)<<a<<endl;
     cout<<"The value of b is: "<<setw(5)<<b<<endl;
     cout<<"The value of c is: "<<setw(5)<<c<<endl;
 
 //operator precendence
-    int a=3, b=6;
-    int c = (((a*b)+10)-b)
+    int x=3, y=6;
+    int z = (((x*y)+10)-y)
     return 0;
 }
