d14fb27b0aa5d7036ec4ccd8a222b5e819ecf1a7
Sanskrati Jain <31sanskrati@gmail.com>
2021-03-08 21:26:02 +0530

    Operators in C++

--- 3-march.cpp
+++ 3-march.cpp
@@ -3,10 +3,10 @@
 // # include "" for adding user defined header like iostream
 using namespace std;
 
-int z = 100;
+int z = 100;//global variable
 
 int main(){
-//*********for running make each section a comment**********
+//*********for running the code--> make each section a comment**********
     int a, b;
     cout<< "Enter the value of a:"<<endl; 
     cin>>a;
@@ -20,10 +20,10 @@ int main(){
     cout<<"The value of * is "<< a*b<<endl;
     cout<<"The value of / is "<<a/b<<endl;
     cout<<"The value of modulo is "<<a%b<<endl;
-    cout<<"The value of increment operator of a is "<<a++<<endl;
-    cout<<"The value of decrement operator of a is "<<a--<<endl;
-    cout<<"The value of increment operator of a is "<<++a<<endl;
-    cout<<"The value of decrement operator of a is "<<--a<<endl;
+    cout<<"The value of post-increment operator of a is "<<a++<<endl;//first output than increment
+    cout<<"The value of post-decrement operator of a is "<<a--<<endl;
+    cout<<"The value of pre-increment operator of a is "<<++a<<endl;//first increment than output
+    cout<<"The value of pre-decrement operator of a is "<<--a<<endl;
     cout<<endl;
 
 //comparision operator
@@ -47,14 +47,14 @@ int main(){
     cin>>x;
     cout<<"Enter the value of y \n";
     cin>>y;
-    z = x + y;
+    z = x + y; //local variable
     cout<<"The sum is "<<z<<endl;
     cout<<"The sum from my brother is "<<::z; //Scope resolution operator --> for printing global value
     cout<<endl;
 
 //For checking what C++ will consider 33.3
-    float d=33.3f;
-    long double e =33.3l;
+    float d=33.3f; //f --> for defining it as float
+    long double e =33.3l; // l --> for long double
     cout<<endl;
     cout<<"The value of d is "<<(int)d<<endl;//or int(d)
     cout<<"The real value of d is "<<(float)d<<endl;
