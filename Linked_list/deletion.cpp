3b2bddb9ebc353d65fd05f140dd9b1590a5bdca8
Sanskrati Jain <31sanskrati@gmail.com>
2021-06-20 18:34:36 +0530

    C++ Practice

---
+++ Linked_list/deletion.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+
+void deleteNode(node **head, int key)
+{
+
+    node *temp = *head;
+    node *prev = NULL;
+
+    if (temp != NULL && temp->data == key)
+    {
+        *head = temp->next;
+        delete temp;
+        return;
+    }
+
+    else
+    {
+        while (temp != NULL && temp->data != key)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        if (temp == NULL)
+        {
+            return;
+        }
+
+        prev->next = temp->next;
+        delete temp;
+    }
+}
+
+void printList(node *head_ref)
+{
+    while (head_ref != NULL)
+    {
+        cout << head_ref->data << " ";
+        head_ref = head_ref->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    node *middle = NULL;
+    node *tail = NULL;
+
+    head = new node();
+    middle = new node();
+    tail = new node();
+
+    head->data = 11;
+    head->next = middle;
+    middle->data = 55;
+    middle->next = tail;
+    tail->data = 110;
+    tail->next = NULL;
+
+    printList(head);
+    deleteNode(&head, 110);
+    printList(head);
+    return 0;
+}
---
+++ Linked_list/deletion_position.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+};
+
+void push(node **head_ref, int new_data)
+{
+    node *new_node = new node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
+}
+
+void printList(node *i)
+{
+    while (i != NULL)
+    {
+        cout << i->data << " ";
+        i = i->next;
+    }
+    cout << endl;
+}
+
+void deleteNode(node **head, int positon){
+    if(*head == NULL){
+        return;
+    }
+    node* temp = *head;
+
+    if(positon == 0){
+        *head = temp->next;
+        free(temp);
+        return;
+    }
+
+    for(int i = 0; temp != NULL && i<positon; i++){
+        temp = temp->next;
+    }
+
+    if(temp == NULL || temp->next == NULL){
+        return;
+    }
+
+    node *next = temp->next->next;
+    free(temp->next);
+    temp->next = next;
+}
+
+int main()
+{
+    node* head = NULL;
+
+    push(&head, 1);
+    push(&head, 2);
+    push(&head, 3);
+    push(&head, 4);
+    push(&head, 5);
+
+    printList(head);
+    deleteNode(&head ,3);
+    printList(head);
+    return 0;
+}
